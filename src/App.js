a8ea7765cfade03b004fb3ffa3581e343f549cd8
cvremil1 <remil.cv@infolitz.com>
2023-08-11 16:39:39 +0530

    counter app compled

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {useState} from 'react'
 
 function App() {
+ const [count, setCount]= useState(0);
+
+ const increment=()=>{
+  setCount(count+1);
+ }
+ const decrement=()=>{
+setCount(count-1);
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="d-flex justify-content-center alignitems-center bg-dark" style={{height:"100vh"}}>
+   <div>
+    <h1 className="text-success text-center">{count}</h1>
+    <div className='d-flex gap-2'>
+      <button className="btn btn-primary"  style={{width:"200px"}} onClick={increment}>+</button>
+      <button className="btn btn-primary"  style={{width:"200px"}} onClick={decrement}>-</button>
     </div>
+   </div>
+  </div>
   );
 }
 
